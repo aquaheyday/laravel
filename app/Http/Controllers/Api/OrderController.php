f5ecad6babd9126776c5613097b3116f7a05599e
ByeongJun Kim <aquaheyday@gmail.com>
2023-11-26 15:29:54 +0900

    order delete

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -56,7 +56,8 @@ class OrderController extends Controller
         })
         ->leftjoin('users', 'users.id', 'orders.user_id')
                 ->select(
-                    'users.name'
+                    'orders.id'
+                    ,'users.name'
                     ,'users.image_path'
                     ,'orders.menu'
                     ,'orders.menu_type'
